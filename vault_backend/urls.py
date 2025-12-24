0c9534cbf740ed6f7391611aee171a3797b0b4a4
Juan <juan.erices04@inacapmail.cl>
2025-12-24 16:27:13 -0300

    Renombrar el módulo de cuentas a "cuentas" en la configuración de URL y actualizar archivos de caché

--- vault_backend/urls.py
+++ vault_backend/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("api/", include("accounts.urls")),
+    path("api/", include("cuentas.urls")),
 ]
