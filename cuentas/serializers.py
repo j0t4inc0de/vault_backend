185cd5bd8a62e6d27407ee5fd45e763b186ba7cb
Juan <juan.erices04@inacapmail.cl>
2026-01-06 21:57:17 -0300

    Crear el Serializador de Registro

--- cuentas/serializers.py
+++ cuentas/serializers.py
@@ -1,7 +1,26 @@
+# cuentas/serializers.py
+
 from rest_framework import serializers
+from django.contrib.auth.models import User
 from .models import Account
 
 class AccountSerializer(serializers.ModelSerializer):
     class Meta:
-        model = Account
+        model = Account 
         exclude = ("user",)
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'password', 'email')
+
+    def create(self, validated_data):
+        # create_user se encarga de hashear la contraseña automáticamente
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            password=validated_data['password'],
+            email=validated_data.get('email', '')
+        )
+        return user
