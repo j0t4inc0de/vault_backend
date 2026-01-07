81e526cd2a2e88196438e730560e0bbe0f1ccef2
Juan <juan.erices04@inacapmail.cl>
2026-01-07 18:18:25 -0300

    Agregar configuración de ALLOWED_HOSTS y CSRF_TRUSTED_ORIGINS basada en la IP del VPS

--- vault_backend/settings/prod.py
+++ vault_backend/settings/prod.py
@@ -1,3 +1,14 @@
 from .base import *
 
 DEBUG = False
+# Leemos la IP desde el archivo .env
+VPS_IP = os.getenv("VPS_IP")
+
+# Si por alguna razón no existe la variable, usamos una lista vacía o fallamos
+if VPS_IP:
+    ALLOWED_HOSTS = [VPS_IP, 'localhost', '127.0.0.1']
+    CSRF_TRUSTED_ORIGINS = [f'http://{VPS_IP}']
+else:
+    # Fallback por seguridad o para debug
+    ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+    CSRF_TRUSTED_ORIGINS = []
