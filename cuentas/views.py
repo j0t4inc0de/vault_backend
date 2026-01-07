dc6be97e11fa7bce6ebabed658286685f41c76e9
Juan <juan.erices04@inacapmail.cl>
2026-01-07 17:15:42 -0300

    Agregar filtros de búsqueda en el conjunto de vistas de cuentas

--- cuentas/views.py
+++ cuentas/views.py
@@ -1,6 +1,6 @@
 # cuentas/views.py
 
-from rest_framework import viewsets, generics
+from rest_framework import viewsets, generics, filters
 from rest_framework.permissions import IsAuthenticated, AllowAny # Importar AllowAny
 from .models import Account
 from .serializers import AccountSerializer, RegisterSerializer # Importar el nuevo serializador
@@ -9,7 +9,9 @@ from django.contrib.auth.models import User
 class AccountViewSet(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
     serializer_class = AccountSerializer
-
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['site_name', 'site_url', 'email'] # Campos donde buscará
+    
     def get_queryset(self):
         return Account.objects.filter(user=self.request.user)
 
