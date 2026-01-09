f4c12a5f772045f11ea3e8d6853c7dbb3d0c4c81
Juan <juan.erices04@inacapmail.cl>
2026-01-08 23:49:13 -0300

    Agregar registro de rutas para VaultFileViewSet en el enrutador de cuentas

--- cuentas/urls.py
+++ cuentas/urls.py
@@ -1,7 +1,7 @@
 from rest_framework.routers import DefaultRouter
-from .views import AccountViewSet
+from .views import AccountViewSet, VaultFileViewSet
 
 router = DefaultRouter()
 router.register("cuentas", AccountViewSet, basename="cuentas")
-
+router.register("files", VaultFileViewSet, basename="files")
 urlpatterns = router.urls
