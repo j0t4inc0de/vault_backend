5b0a6def24c1a15ad946685e07a9f29bac03edb1
Juan <juan.erices04@inacapmail.cl>
2026-01-17 13:17:04 -0300

    Seguridad: Implementación de Argon2id para login de usuarios

--- vault_backend/settings/base.py
+++ vault_backend/settings/base.py
@@ -51,6 +51,13 @@ MIDDLEWARE = [
     "whitenoise.middleware.WhiteNoiseMiddleware",
 ]
 
+PASSWORD_HASHERS = [
+    "django.contrib.auth.hashers.Argon2PasswordHasher",
+    "django.contrib.auth.hashers.PBKDF2PasswordHasher",
+    "django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher",
+    "django.contrib.auth.hashers.BCryptSHA256PasswordHasher",
+]
+
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
